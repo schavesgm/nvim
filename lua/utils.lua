94f1acfe852485bff0b7f095a0bbc6935c8e11af
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-11-04 08:39:01 +0000

    docs: write function documentation

--- lua/utils.lua
+++ lua/utils.lua
@@ -6,6 +6,9 @@ local function is_empty(path)
     return vim.fn.empty(vim.fn.glob(path)) > 0
 end
 
+---Check if a plugin is installed given its name.
+---@param plugin_name string #Name of the plugin to check.
+---@returns boolean #True if the plugin is installed, otherwise False.
 function M.is_plugin_installed(plugin_name)
     local start_path = M.plugins_path .. "/start/" .. plugin_name
     local opt_path   = M.plugins_path .. "/start/" .. plugin_name
