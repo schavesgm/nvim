f912d2cf0a70098810e20d53b11f65d7af6ef737
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-11-04 08:55:12 +0000

    feat: add options module to configuration

--- init.lua
+++ init.lua
@@ -7,11 +7,4 @@ if not vim.tbl_contains(vim.opt.rtp:get(), base_dir) then
     vim.opt.rtp:append(base_dir)
 end
 
-vim.o.smartindent = true                        -- Make indenting smarter again
-vim.o.autoindent  = true                        -- Allow Neovim to infer the indentation level of the line
-vim.o.expandtab   = true                        -- Convert tabs to spaces
-vim.o.tabstop     = 4                           -- Insert 2 spaces for a tab
-vim.o.shiftwidth  = 4                           -- The number of spaces inserted for each indentation
-vim.o.softtabstop = 4
-
 require("bootstrap"):init()
--- lua/bootstrap.lua
+++ lua/bootstrap.lua
@@ -8,9 +8,6 @@ if vim.fn.has "nvim-0.8" ~= 1 then
     vim.cmd "cquit"
 end
 
--- Load the default configuration
-local plugins = require("defaults.plugins")
-
 -- Builtin plugins to deactivate
 local disabled_built_ins = {
     "2html_plugin",
@@ -36,12 +33,14 @@ for _, plugin in ipairs(disabled_built_ins) do
     vim.g["loaded_" .. plugin] = true
 end
 
-
 ---Initialise the configuration.
 function M:init()
 
+    -- Initialise the default configuration
+    require("core.options"):init(require("defaults.options"))
+
     -- Initialise the plugins
-    require("core.plugins"):init(plugins)
+    require("core.plugins"):init(require("defaults.plugins"))
 
 end
 
