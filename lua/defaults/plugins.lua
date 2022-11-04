b32a3f1fb2ac116f1879386b4651fc866e1c5f05
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-11-04 13:33:54 +0000

    feat: include treesitter plugins in the configuration

--- lua/defaults/plugins.lua
+++ lua/defaults/plugins.lua
@@ -1,18 +1,20 @@
 -- File containing all the default plugins of the system
 return {
-    {
-        "wbthomason/packer.nvim",
-    },
-    {
-        "lewis6991/impatient.nvim",
-    },
-    {
-       "nvim-treesitter/nvim-treesitter",
+    -- Initialisation plugins
+    ["lewis6991/impatient.nvim"] = {},
+
+    -- Treesitter plugins
+    ["nvim-treesitter/nvim-treesitter"] = {
         module = "nvim-treesitter",
         setup = function() require("core.lazy_load").on_file_open("nvim-treesitter") end,
         cmd = require("core.lazy_load").treesitter_cmds,
         run = ":TSUpdate",
         config = function() require("plugins.nvim-treesitter") end,
     },
+    ['nvim-treesitter/playground'] = {
+        requires='nvim-treesitter/nvim-treesitter',
+        ensure_dependencies=true,
+        cmd='TSPlaygroundToggle',
+        config=function() require"nvim-treesitter.configs".setup{} end,
+    },
 }
-
