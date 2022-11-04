fab2fe35a0f7f200d9db993ee31400c9dfa988ae
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-11-04 08:53:49 +0000

    feat: add function to check whether neovim is in headless mode or not

--- lua/utils.lua
+++ lua/utils.lua
@@ -18,4 +18,10 @@ function M.is_plugin_installed(plugin_name)
     return true
 end
 
+---Check if neovim is running on headless mode.
+---@returns boolean #True if neovim runs on headless mode, otherwise False
+function M.in_headless()
+    return #vim.api.nvim_list_uis() == 0
+end
+
 return M
