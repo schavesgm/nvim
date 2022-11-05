09d72bdcd977fac631b313f651e40484ffc2acb5
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-11-05 16:13:20 +0000

    feat: eliminate mason cmds as mason-lspconfig requires mason to be loaded

--- lua/core/lazy_load.lua
+++ lua/core/lazy_load.lua
@@ -74,6 +74,5 @@ end
 --
 -- List of commands loading several plugins
 M.treesitter_cmds = {"TSInstall", "TSBufEnable", "TSBufDisable", "TSEnable", "TSDisable", "TSModuleInfo"}
-M.mason_cmds = {"Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog"}
 
 return M
