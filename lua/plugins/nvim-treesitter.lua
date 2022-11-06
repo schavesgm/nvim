72394f42bd6c89001ce0212739984359f5246a6e
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-11-06 16:40:57 +0000

    fix: treesitter handles missing parsers automatically

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -4,6 +4,7 @@ if not present then return end
 treesitter.setup {
     ensure_installed = {"lua"},
     sync_install = true,
+    auto_install = true,
 
     -- Treesitter highlighting module
     highlight = {
@@ -17,18 +18,3 @@ treesitter.setup {
         enable = true,
     },
 }
-
---Define the autocommand to install TreeSitter parser
-local group = vim.api.nvim_create_augroup("TreeSitterInstall", {clear=true})
-vim.api.nvim_create_autocmd("BufEnter", {
-    callback = function()
-        local status_ok, parser = pcall(vim.treesitter.get_parser)
-        if not status_ok then
-            if string.find(parser, "no parser for") then
-                local ft = vim.bo.filetype
-                vim.notify(string.format("Parser for %s language not found. Installing.", ft))
-                vim.cmd(":TSInstall " .. ft)
-            end
-        end
-    end
-})
