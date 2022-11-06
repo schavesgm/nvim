d20908bfebff5908781c070aacc7b01bd78ef26f
Sergio Chaves García-Mascaraque <sergiozteskate@gmail.com>
2022-11-06 19:05:01 +0000

    feat: include Gitsigns lazy-load function

--- lua/core/lazy_load.lua
+++ lua/core/lazy_load.lua
@@ -41,37 +41,22 @@ M.on_file_open = function(plugin_name)
     }
 end
 
--- 
--- M.packer_cmds = {
---   "PackerSnapshot",
---   "PackerSnapshotRollback",
---   "PackerSnapshotDelete",
---   "PackerInstall",
---   "PackerUpdate",
---   "PackerSync",
---   "PackerClean",
---   "PackerCompile",
---   "PackerStatus",
---   "PackerProfile",
---   "PackerLoad",
--- }
+-- Function to lazyload Gitsigns
+M.gitsigns = function()
+  autocmd({ "BufRead" }, {
+    group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
+    callback = function()
+      vim.fn.system("git rev-parse " .. vim.fn.expand "%:p:h")
+      if vim.v.shell_error == 0 then
+        vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
+        vim.schedule(function()
+          require("packer").loader "gitsigns.nvim"
+        end)
+      end
+    end,
+  })
+end
 
--- 
--- M.gitsigns = function()
---   autocmd({ "BufRead" }, {
---     group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
---     callback = function()
---       vim.fn.system("git rev-parse " .. vim.fn.expand "%:p:h")
---       if vim.v.shell_error == 0 then
---         vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
---         vim.schedule(function()
---           require("packer").loader "gitsigns.nvim"
---         end)
---       end
---     end,
---   })
--- end
---
 -- List of commands loading several plugins
 M.treesitter_cmds = {"TSInstall", "TSBufEnable", "TSBufDisable", "TSEnable", "TSDisable", "TSModuleInfo"}
 
