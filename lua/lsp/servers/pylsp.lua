0558aa10608f0b55a0a9437dca1ee794aa631da0
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2022-11-08 16:26:31 +0000

    feat: update python lsp configuration to work standard

--- lua/lsp/servers/pylsp.lua
+++ lua/lsp/servers/pylsp.lua
@@ -1,2 +1,23 @@
 -- Configuration of pylsp
-return {}
+return {
+    settings = {
+        pylsp = {
+            plugins = {
+                pydocstyle = {
+                    enabled = true,
+                    convention = "google",
+                },
+                flake8 = {
+                    enabled = true,
+                    max_line_length = 100,
+                    max_cognitive_complexity = 15,
+                    ignore = {"E203", "E266", "E501", "W503"},
+                },
+                pylint = {
+                    enabled = true,
+                    args = {"--py36-plus", "--py37-plus", "--py38-plus"},
+                }
+            }
+        }
+    }
+}
