d2dc7fb2999ed3bc94e5c06b1ff2741dc2d8089d
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2022-11-11 11:05:24 +0000

    style: add correct tabulation

--- lua/core/lazy_load.lua
+++ lua/core/lazy_load.lua
@@ -43,18 +43,16 @@ end
 
 -- Function to lazyload Gitsigns
 M.gitsigns = function()
-  autocmd({ "BufRead" }, {
-    group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
-    callback = function()
-      vim.fn.system("git rev-parse " .. vim.fn.expand "%:p:h")
-      if vim.v.shell_error == 0 then
-        vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
-        vim.schedule(function()
-          require("packer").loader "gitsigns.nvim"
-        end)
-      end
-    end,
-  })
+    autocmd({ "BufRead" }, {
+        group = vim.api.nvim_create_augroup("GitSignsLazyLoad", { clear = true }),
+        callback = function()
+            vim.fn.system("git rev-parse " .. vim.fn.expand "%:p:h")
+            if vim.v.shell_error == 0 then
+                vim.api.nvim_del_augroup_by_name "GitSignsLazyLoad"
+                vim.schedule(function() require("packer").loader "gitsigns.nvim" end)
+            end
+        end,
+    })
 end
 
 -- List of commands loading several plugins
