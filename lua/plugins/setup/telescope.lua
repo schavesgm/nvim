a5fd47f4c4f9abab1c523bfdc8b4e605474de562
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2022-12-08 17:12:54 +0000

    feat: include warning when ripgrep is not installed

--- lua/plugins/setup/telescope.lua
+++ lua/plugins/setup/telescope.lua
@@ -1,3 +1,9 @@
+if not vim.fn.executable("rgrep") then
+    vim.notify("ripgrep is not installed, please run:", vim.log.levels.WARN)
+    vim.notify("sudo apt install ripgrep", vim.log.levels.WARN)
+end
+
+
 vim.keymap.set('n', '<leader>ff', "<cmd>Telescope find_files<cr>", {noremap = true, silent = true})
 vim.keymap.set('n', '<leader>fg', "<cmd>Telescope live_grep<cr>", {noremap = true, silent = true})
 vim.keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<cr>", {noremap = true, silent = true})
