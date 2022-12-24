a942d546c0d5e9adcf24216350788792276bfc0c
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2022-12-24 12:19:30 +0000

    feat(telescope): include mapping to open quickfix window

--- lua/plugins/setup/telescope.lua
+++ lua/plugins/setup/telescope.lua
@@ -3,7 +3,8 @@ if not vim.fn.executable("rgrep") then
     vim.notify("sudo apt install ripgrep", vim.log.levels.WARN)
 end
 
-
-vim.keymap.set('n', '<leader>ff', "<cmd>Telescope find_files<cr>", {noremap = true, silent = true})
-vim.keymap.set('n', '<leader>fg', "<cmd>Telescope live_grep<cr>", {noremap = true, silent = true})
-vim.keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<cr>", {noremap = true, silent = true})
+local opts = {noremap=true, silent=true}
+vim.keymap.set('n', '<leader>ff', "<cmd>Telescope find_files<cr>", opts)
+vim.keymap.set('n', '<leader>fg', "<cmd>Telescope live_grep<cr>", opts)
+vim.keymap.set('n', '<leader>fb', "<cmd>Telescope buffers<cr>", opts)
+vim.keymap.set('n', '<leader>fq', "<cmd>Telescope quickfix<cr>", opts)
