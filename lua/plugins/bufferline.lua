9747cd8890b215f18cd3bad02294fc2480b68886
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-01-01 16:14:13 +0000

    feat(bufferline): add bufferline option for FileExplorere

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -8,7 +8,18 @@ function M.config()
     local is_present, plugin = pcall(require, "bufferline")
     if not is_present then return end
 
-    plugin.setup{}
+    plugin.setup{
+        options = {
+            offsets = {
+                 {
+                    filetype = "NvimTree",
+                    text = "File Explorer",
+                    text_align = "center",
+                    separator = true
+                }
+            },
+        }
+    }
 end
 
 return M
