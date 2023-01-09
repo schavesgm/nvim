03ac08b8d52941a7f2da6ffecba70b61cf7b22e7
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-01-09 16:13:26 +0000

    feat(lsp:pylsp): add support for mypy

--- lua/lsp/servers/pylsp.lua
+++ lua/lsp/servers/pylsp.lua
@@ -2,30 +2,33 @@
 return {
     settings = {
         pylsp = {
-            configurationSources = {"flake8"},
+            configurationSources = { "flake8" },
             plugins = {
                 flake8 = {
                     enabled = true,
                     max_line_length = 100,
                     max_cognitive_complexity = 15,
-                    ignore = {"E203", "E266", "E501", "E303", "W503", "E302"},
+                    ignore = { "E203", "E266", "E501", "E303", "W503", "E302" },
                 },
                 pyflakes = {
                     enabled = false,
                 },
                 pylint = {
                     enabled = true,
-                    args = {"--py36-plus", "--py37-plus", "--py38-plus"},
+                    args = { "--py36-plus", "--py37-plus", "--py38-plus" },
                 },
                 pycodestyle = {
                     enabled = false,
                 },
-                pylsp_mypy = {
-                    enabled = true,
-                },
                 pylsp_black = {
                     enabled = true,
                     line_length = 100,
+                },
+                pylsp_isort = {
+                    enabled = true,
+                },
+                pylsp_mypy = {
+                    enabled = true,
                 }
                 -- For some reason, this eliminates all other diagnostics
                 -- pydocstyle = {
