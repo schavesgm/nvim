b9bb367bacae6f870fdd386efe771540de85cfab
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-01-09 13:23:00 +0000

    fix(pylsp): update pylsp plugins to show diagnostics

--- lua/lsp/servers/pylsp.lua
+++ lua/lsp/servers/pylsp.lua
@@ -1,25 +1,34 @@
 -- Configuration of pylsp
+print("I am here")
 return {
     settings = {
         pylsp = {
+            configurationSources = {"flake8"},
             plugins = {
-                pydocstyle = {
-                    enabled = true,
-                    convention = "google",
-                },
                 flake8 = {
                     enabled = true,
                     max_line_length = 100,
                     max_cognitive_complexity = 15,
                     ignore = {"E203", "E266", "E501", "E303", "W503", "E302"},
                 },
+                pyflakes = {
+                    enabled = false,
+                },
                 pylint = {
                     enabled = true,
                     args = {"--py36-plus", "--py37-plus", "--py38-plus"},
                 },
                 pycodestyle = {
-                    enabled = false,
+                    enabled = true,
+                },
+                pylsp_mypy = {
+                    enabled = true,
                 }
+                -- For some reason, this eliminates all other diagnostics
+                -- pydocstyle = {
+                --     enabled = true,
+                --     convention = "google",
+                -- },
             }
         }
     }
