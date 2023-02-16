f2d52b02cea1786b9017b93e7082c5890b5e44a0
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-02-16 17:32:31 +0000

    feat(lsp): update configuration

--- lua/lsp/servers/pylsp.lua
+++ lua/lsp/servers/pylsp.lua
@@ -2,76 +2,29 @@
 return {
     settings = {
         pylsp = {
-            configurationSources = { "flake8" },
+            configurationSources = {"flake8"},
             plugins = {
                 -- Jedi plugin
-                jedi_completion = {
-                    enabled = true,
-                },
-                jedi_definition = {
-                    enabled = true,
-                },
-                jedi_hover = {
-                    enabled = true,
-                },
-                jedi_references = {
-                    enabled = true,
-                },
-                jedi_signature_help = {
-                    enabled = true,
-                },
-                jedi_symbols = {
-                    enabled = true,
-                },
-                -- Autopep8
-                autopep8 = {
-                    enabled = false,
-                },
+                jedi_completion = {enabled = true},
+                jedi_definition = {enabled = true},
+                jedi_hover = {enabled = true},
+                jedi_references = {enabled = true},
+                jedi_signature_help = {enabled = true},
+                jedi_symbols = {enabled = true},
                 -- McCabe
-                mccabe = {
-                    enabled = true,
-                    threshold = 15,
-                },
+                mccabe = {enabled = true, threshold = 15},
                 -- Preload
-                preload = {
-                    enabled = false,
-                },
+                preload = {enabled = false},
                 -- PyCodestyle
                 pycodestyle = {
                     enabled = true,
                     exclude = {"__init__.py"},
-                    maxLineLength = 100,
+                    maxLineLength = 100
                 },
                 -- PyDocStyle
-                pydocstyle = {
-                    enabled = true,
-                    convention = "google",
-                },
+                pydocstyle = {enabled = true, convention = "google"},
                 -- PyFlakes
-                pyflakes = {
-                    enabled = true,
-                },
-                -- PyLint
-                pylint = {
-                    enabled = false,
-                },
-                -- Flake8
-                flake8 = {
-                    enabled = false,
-                },
-                -- MyPY
-                pylsp_mypy = {
-                    enabled = true,
-                },
-                -- Black
-                pylsp_black = {
-                    enabled = true,
-                    line_length = 100,
-                },
-                -- Isort
-                pylsp_isort = {
-                    enabled = true,
-                },
+                pyflakes = {enabled = true}
             }
         }
     }
