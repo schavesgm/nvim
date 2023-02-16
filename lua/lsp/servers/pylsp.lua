ca88ca5118c86167baf1651dee5e18ca56713f8d
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-02-16 17:32:51 +0000

    refactor(lsp): format file

--- lua/lsp/servers/pylsp.lua
+++ lua/lsp/servers/pylsp.lua
@@ -2,29 +2,29 @@
 return {
     settings = {
         pylsp = {
-            configurationSources = {"flake8"},
+            configurationSources = { "flake8" },
             plugins = {
                 -- Jedi plugin
-                jedi_completion = {enabled = true},
-                jedi_definition = {enabled = true},
-                jedi_hover = {enabled = true},
-                jedi_references = {enabled = true},
-                jedi_signature_help = {enabled = true},
-                jedi_symbols = {enabled = true},
+                jedi_completion = { enabled = true },
+                jedi_definition = { enabled = true },
+                jedi_hover = { enabled = true },
+                jedi_references = { enabled = true },
+                jedi_signature_help = { enabled = true },
+                jedi_symbols = { enabled = true },
                 -- McCabe
-                mccabe = {enabled = true, threshold = 15},
+                mccabe = { enabled = true, threshold = 15 },
                 -- Preload
-                preload = {enabled = false},
+                preload = { enabled = false },
                 -- PyCodestyle
                 pycodestyle = {
                     enabled = true,
-                    exclude = {"__init__.py"},
+                    exclude = { "__init__.py" },
                     maxLineLength = 100
                 },
                 -- PyDocStyle
-                pydocstyle = {enabled = true, convention = "google"},
+                pydocstyle = { enabled = true, convention = "google" },
                 -- PyFlakes
-                pyflakes = {enabled = true}
+                pyflakes = { enabled = true }
             }
         }
     }
