3fc359f9cec1bcf0f07fd188d949d8d2800f759c
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-02-16 17:31:26 +0000

    style(core): format file

--- lua/core/autocmds.lua
+++ lua/core/autocmds.lua
@@ -4,20 +4,19 @@ local M = {}
 ---@param autocommands table #Table containing the augroups and their autocommands.
 function M.set_augroup_autocmds(autocommands)
     for augroup, autocmds in pairs(autocommands) do
-        local group = vim.api.nvim_create_augroup(augroup, {clear=true})
+        local group = vim.api.nvim_create_augroup(augroup, { clear = true })
         for _, autocmd in ipairs(autocmds) do
-            vim.api.nvim_create_autocmd(
-                autocmd.event, 
-                vim.tbl_extend("keep", autocmd.opts, {group=group})
-            )
+            vim.api.nvim_create_autocmd(autocmd.event, vim.tbl_extend("keep",
+                autocmd.opts,
+                {
+                    group = group
+                }))
         end
     end
 end
 
 ---Set the default autocommands on the system
 ---@param options table #Table containing all the autogroups and autocommands of the system
-function M:init(options)
-    M.set_augroup_autocmds(options)
-end
+function M:init(options) M.set_augroup_autocmds(options) end
 
 return M
