d0bd2a8d7fd54ef558bee47a1562439365c2d6c4
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-02-20 21:11:50 +0000

    feat(defaults): add autocommand to install main Python-dev dependencies

--- lua/defaults/autocmds.lua
+++ lua/defaults/autocmds.lua
@@ -4,8 +4,8 @@ local timers = {}
 -- Wrapper around a callback function to generate a timed loop
 local function start_timed_callback(callback, start, every)
     return function()
-        local bufnr   = vim.api.nvim_buf_get_name(0)
-        local lambda  = vim.schedule_wrap(function() callback(bufnr) end)
+        local bufnr = vim.api.nvim_buf_get_name(0)
+        local lambda = vim.schedule_wrap(function() callback(bufnr) end)
         timers[bufnr] = vim.loop.new_timer()
         timers[bufnr]:start(start, every, lambda)
     end
@@ -24,50 +24,86 @@ local function end_timed_callback()
 end
 
 -- Small function to convert minutes to miliseconds
-local function minutes_to_milisecodns(minutes)
-    return minutes * 60 * 1000
-end
+local function minutes_to_milisecodns(minutes) return minutes * 60 * 1000 end
 
 -- Table containing all autogroups/autocommands definitions
 return {
-    RestoreCursor = {
-        { event = "BufRead", opts = { pattern = "*", command = [[call setpos(".", getpos("'\""))]] } }
+    BaseDefaults = {
+        {
+            event = "BufRead",
+            opts = {
+                pattern = "*",
+                command = [[call setpos(".", getpos("'\""))]]
+            }
+        }, { event = "VimLeave", opts = { pattern = "*", command = "wshada!" } }
     },
-    TerminalCommands = {
-        { event = "TermOpen", opts = { pattern = "*", command = [[tnoremap <buffer> <Esc> <c-\><c-n>]] } },
+    TerminalDefaults = {
+        {
+            event = "TermOpen",
+            opts = {
+                pattern = "*",
+                command = [[tnoremap <buffer> <Esc> <c-\><c-n>]]
+            }
+        },
         { event = "TermOpen", opts = { pattern = "*", command = "startinsert" } },
-        { event = "TermOpen", opts = { pattern = "*", command = [[setlocal nonumber norelativenumber]] } },
-        { event = "TermOpen", opts = { pattern = "*", command = [[setlocal filetype=term]] } },
-        { event = "TermOpen", opts = { pattern = "*", command = [[setlocal spell!]] } },
+        {
+            event = "TermOpen",
+            opts = {
+                pattern = "*",
+                command = [[setlocal nonumber norelativenumber]]
+            }
+        }, {
+            event = "TermOpen",
+            opts = { pattern = "*", command = [[setlocal filetype=term]] }
+        },
+        {
+            event = "TermOpen",
+            opts = { pattern = "*", command = [[setlocal spell!]] }
+        }
     },
-    SaveToShada = {
-        { event = "VimLeave", opts = { pattern = "*", command = "wshada!" } },
-    },
-    MarkupAutocmds = {
+    MarkupDefaults = {
         {
             event = { "BufEnter", "WinEnter" },
             opts = {
                 pattern = { "*.tex", "*.txt", "*.md" },
                 command = [[setlocal textwidth=100 colorcolumn=+1]]
             }
-        },
-        {
+        }, {
             event = "BufEnter",
             opts = {
                 pattern = { "*.tex", "*.md" },
-                callback = start_timed_callback(
-                    function(bufnr) vim.cmd(':w ' .. bufnr) end,
-                    minutes_to_milisecodns(3),
-                    minutes_to_milisecodns(3)
-                )
-            },
-        },
-        {
-            event = "BufLeave",
-            opts = {
-                pattern = { "*.tex", "*.md" },
-                callback = end_timed_callback,
+                callback = start_timed_callback(function(bufnr)
+                    vim.cmd(':w ' .. bufnr)
+                end, minutes_to_milisecodns(3), minutes_to_milisecodns(3))
             }
+        }, {
+            event = "BufLeave",
+            opts = { pattern = { "*.tex", "*.md" }, callback = end_timed_callback }
         }
     },
+    PythonDefaults = {
+        {
+            event = { "FileType" },
+            opts = {
+                pattern = "python",
+                callback = function()
+                    local install_python_dev = function()
+                        local packages = {
+                            "flake8", "pep8-naming", "flake8-builtins",
+                            "flake8-comprehensions", "flake8-bugbear",
+                            "flake8-pytest-style",
+                            "flake8-cognitive-complexity", "pydocstyle",
+                            "pylint", "pyupgrade", "prettier", "mypy", "black",
+                            "isort"
+                        }
+                        vim.cmd("!pip install --upgrade pip")
+                        for _, package in ipairs(packages) do
+                            vim.cmd(string.format("!pip install %s", package))
+                        end
+                    end
+                    vim.api.nvim_create_user_command("InstallPythonDevLsp", install_python_dev, {})
+                end
+            }
+        }
+    }
 }
