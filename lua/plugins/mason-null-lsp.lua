1422585c5436c3a136ceef8282ea240f81c0d7c2
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-02-23 10:20:48 +0000

    feat(plugins): include null-lsp mason bridge plugin

---
+++ lua/plugins/mason-null-lsp.lua
@@ -0,0 +1,28 @@
+local M = {
+	"jay-babu/mason-null-ls.nvim",
+	dependencies = {
+		"jose-elias-alvarez/null-ls.nvim",
+		"williamboman/mason.nvim",
+	},
+}
+
+function M.config()
+	local is_mason_null_present, mason_null = pcall(require, "mason-null-ls")
+	if not is_mason_null_present then
+		return
+	end
+	local is_null_present, null_ls = pcall(require, "null-ls")
+	if not is_null_present then
+		return
+	end
+
+	mason_null.setup({ automatic_setup = true, sources = {} })
+
+	mason_null.setup_handlers({
+		stylua = function(_, _)
+			null_ls.register(null_ls.builtins.formatting.stylua)
+		end,
+	})
+end
+
+return M
