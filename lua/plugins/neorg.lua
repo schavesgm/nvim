1d3c1dad81f40754b0bb48b155f9e86340c7b099
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-03-08 13:06:59 +0000

    feat(plugins): add neorg configuration

---
+++ lua/plugins/neorg.lua
@@ -0,0 +1,35 @@
+local M = {
+	"nvim-neorg/neorg",
+	build = ":Neorg sync-parsers",
+	opts = {
+		load = {
+			["core.defaults"] = {}, -- Loads default behaviour
+            ["core.fs"] = {},
+            ["core.keybinds"] = {
+                config = {
+                    defaults_keybinds = true,
+                }
+            },
+			["core.norg.concealer"] = {},
+			["core.norg.completion"] = {
+                config = {
+                    engine = "nvim-cmp",
+                }
+            },
+			["core.norg.dirman"] = {
+				config = {
+                    default_workspace = "tasks",
+					workspaces = {
+						tasks = "~/Documents/neorg/tasks",
+					},
+                    index = "index.norg",
+                    use_popup = true,
+				},
+			},
+            ["core.norg.esupports.hop"] = {},
+		},
+	},
+	dependencies = { { "nvim-lua/plenary.nvim" } },
+}
+
+return M
