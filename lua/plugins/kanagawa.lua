a5a1b1ac618a1940e0b460d79bdce3e227967b10
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-03-08 13:18:11 +0000

    fix(plugins): adapt code to new kanagawa plugins

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -15,13 +15,13 @@ function M.config()
 
     -- Set some custom highlights on this colourscheme
     local colors = require("kanagawa.colors").setup()
-    vim.api.nvim_set_hl(0, 'WinSeparator',               {bg=nil, fg=colors.crystalBlue, bold=true})
-    vim.api.nvim_set_hl(0, 'IndentBlanklineChar',        {bg=nil, fg=colors.sumiInk2, bold=true})
-    vim.api.nvim_set_hl(0, 'IndentBlanklineContextChar', {bg=nil, fg=colors.oniViolet, bold=true})
-    vim.api.nvim_set_hl(0, 'DiagnosticError',            {bg=nil, fg=colors.springViolet1, bold=false})
-    vim.api.nvim_set_hl(0, 'DiagnosticWarn',             {bg=nil, fg=colors.springViolet1, bold=false})
-    vim.api.nvim_set_hl(0, 'DiagnosticInfo',             {bg=nil, fg=colors.springViolet1, bold=false})
-    vim.api.nvim_set_hl(0, 'DiagnosticHint',             {bg=nil, fg=colors.springViolet1, bold=false})
+    vim.api.nvim_set_hl(0, 'WinSeparator',               {bg=nil, fg=colors.palette.crystalBlue, bold=true})
+    vim.api.nvim_set_hl(0, 'IndentBlanklineChar',        {bg=nil, fg=colors.palette.sumiInk2, bold=true})
+    vim.api.nvim_set_hl(0, 'IndentBlanklineContextChar', {bg=nil, fg=colors.palette.oniViolet, bold=true})
+    vim.api.nvim_set_hl(0, 'DiagnosticError',            {bg=nil, fg=colors.palette.springViolet1, bold=false})
+    vim.api.nvim_set_hl(0, 'DiagnosticWarn',             {bg=nil, fg=colors.palette.springViolet1, bold=false})
+    vim.api.nvim_set_hl(0, 'DiagnosticInfo',             {bg=nil, fg=colors.palette.springViolet1, bold=false})
+    vim.api.nvim_set_hl(0, 'DiagnosticHint',             {bg=nil, fg=colors.palette.springViolet1, bold=false})
 end
 
 return M
