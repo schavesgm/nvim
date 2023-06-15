10345c2bb262774e2a3037c0ac6b0e4130950874
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-06-15 10:40:12 +0100

    feat(lsp): eliminate unrequired language server

--- lua/lsp/servers/init.lua
+++ lua/lsp/servers/init.lua
@@ -2,7 +2,7 @@
 local M = {}
 
 -- Add all the servers
-local servers = {"jsonls", "lua_ls", "pylsp", "texlab", "efm"}
+local servers = {"jsonls", "lua_ls", "pylsp", "texlab"}
 
 for _, name in ipairs(servers) do
     M[name] = require(string.format("lsp.servers.%s", name))
