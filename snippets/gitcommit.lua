53cc48cf6f9b48342e20b3cd251e35395d5a268b
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-06-30 12:58:17 +0100

    feat(snippets): add more scope entries

--- snippets/gitcommit.lua
+++ snippets/gitcommit.lua
@@ -1,11 +1,22 @@
 local snippets = require("luasnip")
 local format = require("luasnip.extras.fmt").fmt
 
-return {
-    snippets.s(
-        "rebase", format("rebase({}): {}", {
-            snippets.i(1, "scope"),
-            snippets.i(2, "message"),
-        })
-    ),
-}
+local function generate_new_commit_message(scope)
+    return snippets.s(
+        scope,
+        format(
+            string.format("%s({}): {}", scope),
+            {
+                snippets.i(1, "scope"),
+                snippets.i(2, "message"),
+            }
+        )
+    )
+end
+
+local entries = {}
+for _, scope in ipairs({"rebase", "fixup"}) do
+    entries[#entries + 1] = generate_new_commit_message(scope)
+end
+
+return entries
