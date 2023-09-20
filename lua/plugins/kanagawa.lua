e0c0e115cbc9a41a0b05621acd6b4d65939fbba4
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-09-20 16:44:06 +0100

    feat(plugins): add custom colours for floating windows

--- lua/plugins/kanagawa.lua
+++ lua/plugins/kanagawa.lua
@@ -15,13 +15,15 @@ function M.config()
 
     -- Set some custom highlights on this colourscheme
     local colors = require("kanagawa.colors").setup()
-    vim.api.nvim_set_hl(0, 'WinSeparator',               {bg=nil, fg=colors.palette.crystalBlue, bold=true})
-    vim.api.nvim_set_hl(0, 'IndentBlanklineChar',        {bg=nil, fg=colors.palette.sumiInk2, bold=true})
-    vim.api.nvim_set_hl(0, 'IndentBlanklineContextChar', {bg=nil, fg=colors.palette.oniViolet, bold=true})
-    vim.api.nvim_set_hl(0, 'DiagnosticError',            {bg=nil, fg=colors.palette.springViolet1, bold=false})
-    vim.api.nvim_set_hl(0, 'DiagnosticWarn',             {bg=nil, fg=colors.palette.springViolet1, bold=false})
-    vim.api.nvim_set_hl(0, 'DiagnosticInfo',             {bg=nil, fg=colors.palette.springViolet1, bold=false})
-    vim.api.nvim_set_hl(0, 'DiagnosticHint',             {bg=nil, fg=colors.palette.springViolet1, bold=false})
+    vim.api.nvim_set_hl(0, 'WinSeparator', { bg = nil, fg = colors.palette.crystalBlue, bold = true })
+    vim.api.nvim_set_hl(0, 'IndentBlanklineChar', { bg = nil, fg = colors.palette.sumiInk2, bold = true })
+    vim.api.nvim_set_hl(0, 'IndentBlanklineContextChar', { bg = nil, fg = colors.palette.oniViolet, bold = true })
+    vim.api.nvim_set_hl(0, 'DiagnosticError', { bg = nil, fg = colors.palette.springViolet1, bold = false })
+    vim.api.nvim_set_hl(0, 'DiagnosticWarn', { bg = nil, fg = colors.palette.springViolet1, bold = false })
+    vim.api.nvim_set_hl(0, 'DiagnosticInfo', { bg = nil, fg = colors.palette.springViolet1, bold = false })
+    vim.api.nvim_set_hl(0, 'DiagnosticHint', { bg = nil, fg = colors.palette.springViolet1, bold = false })
+    vim.api.nvim_set_hl(0, 'FloatBorder', { bg = nil, fg = colors.palette.oniViolet, bold = true })
+    vim.api.nvim_set_hl(0, 'NormalFloat', { bg = nil, fg = colors.palette.fujiWhite, bold = false })
 end
 
 return M
