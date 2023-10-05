806c08422c4f603d361fe179eb5b31c53cba3f5c
Sergio Chaves García-Mascaraque <s.garciamascaraque@instadeep.com>
2023-10-05 13:52:16 +0100

    feat(plugins): support indent-blankline v3.0

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -3,45 +3,17 @@ local M = {
 }
 
 function M.init()
-    vim.keymap.set("n", "<leader>cc",
-        function()
-            local ok, start = require("indent_blankline.utils").get_current_context(
-                vim.g.indent_blankline_context_patterns,
-                vim.g.indent_blankline_use_treesitter_scope
-            )
-
-            if ok then
-                vim.api.nvim_win_set_cursor(vim.api.nvim_get_current_win(), { start, 0 })
-                vim.cmd [[normal! _]]
-            end
-        end
-    )
+    local is_kanagawa_present, _ = pcall(require, "kanagawa")
+    if not is_kanagawa_present then return end
+    local colours = require("kanagawa.colors").setup().palette
+    vim.api.nvim_set_hl(0, "IblIndent", { bg = nil, fg = colours.sumiInk4, bold = false })
+    vim.api.nvim_set_hl(0, "IblScope", { bg = nil, fg = colours.springViolet1, bold = false })
 end
 
 function M.config()
-    local is_present, plugin = pcall(require, "indent_blankline")
+    local is_present, plugin = pcall(require, "ibl")
     if not is_present then return end
-
-    plugin.setup({
-        indentLine_enabled = 1,
-        filetype_exclude = {
-            "help",
-            "terminal",
-            "alpha",
-            "packer",
-            "lspinfo",
-            "TelescopePrompt",
-            "TelescopeResults",
-            "mason",
-            "",
-        },
-        buftype_exclude = { "terminal" },
-        show_trailing_blankline_indent = false,
-        show_first_indent_level = false,
-        space_char_blankline = " ",
-        show_current_context = true,
-        show_current_context_start = true,
-    })
+    plugin.setup()
 end
 
 return M
